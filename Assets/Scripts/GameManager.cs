ab365f212ecf141ff43a3fa14bbb9cba31b269f7
andrewjsliang <andrewjsliang@gmail.com>
2022-08-25 19:17:00 -0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int world { get; private set; }
+    public int stage { get; private set; }
+    public int lives { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null) {
+            DestroyImmediate(gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this) {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        lives = 3;
+
+        LoadLevel(1, 1);
+    }
+
+    private void LoadLevel(int world, int stage)
+    {
+        this.world = world;
+        this.stage = stage;
+
+        SceneManager.LoadScene($"{world}-{stage}");
+    }
+
+    public void NextLevel()
+    {
+        LoadLevel(world, stage + 1);
+    }
+
+    public void ResetLevel(float delay)
+    {
+        Invoke(nameof(ResetLevel), delay);
+    }
+
+    public void ResetLevel()
+    {
+        lives--;
+
+        if (lives > 0) {
+            LoadLevel(world, stage);
+        } else {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        Invoke(nameof(NewGame), 3f);
+    }
+
+}
---
+++ Assets/Scripts/SideScrolling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+}
